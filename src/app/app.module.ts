c5f807323ac3660dbaf6913c837c50978ea089e9
Tran Hoang Long <hoanglong24_@MacBook-Pro-cua-Long.local>
2021-10-03 14:02:02 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/pages/header/header.component';
+import { FooterComponent } from './components/pages/footer/footer.component';
 import { DashboardComponent } from './components/admin/dashboard/dashboard.component';
 import { LoginComponent } from './components/admin/login/login.component';
 import { LoginUserComponent } from './components/user/login-user/login-user.component';
