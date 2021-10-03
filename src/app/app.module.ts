3e96cae4005c0d893b8c264286819d1376b51c00
Tran Hoang Long <hoanglong24_@MacBook-Pro-cua-Long.local>
2021-10-03 12:54:12 +0700

    v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { DashboardComponent } from './components/admin/dashboard/dashboard.component';
+import { LoginComponent } from './components/admin/login/login.component';
+import { LoginUserComponent } from './components/user/login-user/login-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    DashboardComponent,
+    LoginComponent,
+    LoginUserComponent
   ],
   imports: [
     BrowserModule,
