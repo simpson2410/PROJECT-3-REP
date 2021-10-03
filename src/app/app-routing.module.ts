d405ee252f66cfd020bd496da0e1e37c40f7d870
Tran Hoang Long <hoanglong24_@MacBook-Pro-cua-Long.local>
2021-10-03 17:07:46 +0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginUserComponent } from './components/user/login-user/login-user.component';
+import { HomeComponent } from './components/pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'Login-User', component: LoginUserComponent },
+  { path: '', component: HomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module'; // CLI imports AppRoutingModule
 import { AppComponent } from './app.component';
+
 import { HeaderComponent } from './components/pages/header/header.component';
 import { FooterComponent } from './components/pages/footer/footer.component';
 import { DashboardComponent } from './components/admin/dashboard/dashboard.component';
 import { LoginComponent } from './components/admin/login/login.component';
 import { LoginUserComponent } from './components/user/login-user/login-user.component';
 import { CarouselComponent } from './components/pages/carousel/carousel.component';
+import { HomeComponent } from './components/pages/home/home.component';
 
 @NgModule({
   declarations: [
@@ -19,6 +20,7 @@ import { CarouselComponent } from './components/pages/carousel/carousel.componen
     LoginComponent,
     LoginUserComponent,
     CarouselComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
